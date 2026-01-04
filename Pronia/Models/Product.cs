675eb14a5557c7aa15346265e9a20483b25d800e
IslamOsmanov <159237806+IslamOsmanov@users.noreply.github.com>
2026-01-04 23:55:00 +0400

    Multiple data upload and detail page

--- Pronia/Models/Product.cs
+++ Pronia/Models/Product.cs
@@ -5,27 +5,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pronia.Models;
 
-public class  Product:BaseEntity
+public class Product : BaseEntity
 {
-    [Required]
-    public string Name { get; set; }
-    [Required]
-    public string Description { get; set; }
-    
-    [Required]
-    [Precision(10,2)]
-    [Range(0,double.MaxValue)]
-    public decimal Price { get; set; }
-    public Category? Category { get; set; }
-    [Required]
-    public int CategoryId { get; set; }
-    public string MainImagePath { get; set; }
-    public string HoverImagePath { get; set; }
-    public ICollection<ProductImage> ProductImages { get; set; } = [];
-    public ICollection<ProductTag> ProductTags { get; set; } = [];
+	[Required]
+	public string Name { get; set; }
+	[Required]
+	public string Description { get; set; }
 
-
-
-    [Range(1, 5)]
-    public double? Rating { get; set; }
-} 
+	[Required]
+	[Precision(10, 2)]
+	[Range(0, double.MaxValue)]
+	public decimal Price { get; set; }
+	public Category Category { get; set; }
+	[Required]
+	public int CategoryId { get; set; }
+	public string MainImagePath { get; set; }
+	public string HoverImagePath { get; set; }
+	public ICollection<ProductImage> ProductImages { get; set; } = [];
+	public ICollection<ProductTag> ProductTags { get; set; } = [];
+	[Range(1, 5)]
+	public double? Rating { get; set; }
+}
